11d688d44b9dc4301e979b0bbea09d43867a7b7b
AlessandroMondelli <alexs98@hotmail.it>
2021-12-23 18:49:25 +0100

    Iniziata struttura cartelle, aggiunto bootstrap

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  Footer File
+ *  @package theminimal
+ */
+
+?>
+
+    <hr>
+    <footer id="theminimal-footer">
+        <h2>Footer Tema</h2>
+        <?php wp_footer(); ?>
+    </footer>
+</body>
--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 /**
   *   Main template file
+  *
   *   @package theminimal
   */
 
+
+get_header();
+
+get_footer();
