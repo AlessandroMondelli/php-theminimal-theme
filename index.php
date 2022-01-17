6f2c288aa85882018b64f7f51954e7f7b7800d67
Alessandro Mondelli <alexs98@hotmail.it>
2022-01-17 12:48:30 +0100

    Inizio studio ed utilizzo di The Loop, stampati a schermo i post nella pagina blog; Da rendere cliccabili i titoli e inserire supporto per immagini di copertina

--- index.php
+++ index.php
@@ -7,5 +7,41 @@
 
 
 get_header();
+?>
 
+<div id="theminimal-blog-main">
+  <?php
+  if( have_posts() ) {
+  ?>
+    <div class="container">
+      <?php
+      if( is_home() && !is_front_page() ) {
+        ?>
+        <div class="theminimal-page-title">
+          <h1><?php single_post_title() ?></h1>
+        </div>
+        <?php
+      }
+      ?>
+      <div class="row">
+      <?php
+      while( have_posts() ) : the_post();
+      ?>
+        <div class="col-lg-4 col-md-6 col-sm-12">
+          <?php
+          get_template_part( 'template-parts/blog/content' );
+        
+        ?>
+        </div>
+        <?php
+        endwhile;
+        ?>
+      </div>
+    </div> 
+  <?php
+  }
+  ?>
+</div>
+
+<?php
 get_footer();
---
+++ template-parts/blog/content.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Post content template
+ * 
+ * @package theminimal
+ */
+
+?>
+
+<div class="theminimal-single-post">
+    <h2><?php the_title() ?></h2>
+    <p><?php the_excerpt() ?></p>
+</div>
