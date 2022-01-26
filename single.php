2dfccadf843cfcf74d1fb96c7620ac7bfe5efd85
Alessandro Mondelli <alexs98@hotmail.it>
2022-01-26 18:14:23 +0100

    Inserito contenuto nel single.php, ora i post sono visualizzabili; implementato supporto a paginazione per single post

--- single.php
+++ single.php
@@ -9,7 +9,20 @@ get_header(); //Recupero header
 ?>
 
 <div id="theminimal-single-post">
-    <h1>Post title</h1>
+    <?php
+        if( have_posts() ) {
+            if( is_home() && !is_front_page()) {   
+    ?>
+                <div class="theminimal-single-title">
+                    <h1><?php single_post_title(); ?></h1>
+                </div>
+    <?php
+            }
+            while ( have_posts() ) : the_post();
+                get_template_part( 'template-parts/blog/content' );
+            endwhile;
+        }
+    ?>
 </div>
 
 <?php
--- template-parts/blog/components/entry-content.php
+++ template-parts/blog/components/entry-content.php
@@ -23,6 +23,13 @@
                     the_title( '<span class="screen-reader-text">', '</span>', false )
                 )
             );
+        
+            wp_link_pages(
+                [
+                    'before' => '<div class="page-links">' . esc_html__( 'Pagina:', 'theminimal' ),
+                    'after' => '</div>'
+                ]
+            );
         } else {
             theminimal_the_excerpt( 150 ); //Richiamo funzione che taglia estratto
             theminimal_excerpt_more();
